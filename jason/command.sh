848685ba380cb2e22efb9b99c6950f0195688ef2
Josh Wentz <joshwentz@gmail.com>
2014-02-13 22:39:54 -0500

    Ruby Command Equivalent updated to match buildingData.json

--- jason/command.sh
+++ jason/command.sh
@@ -6,7 +6,7 @@ xvfb-run -a node openstudio-run.js;
 # to INSTALL xvfb on Ubuntu:  sudo apt-get install xvfb
 
 # RUBY COMMAND EQUIVALENT
-# xvfb-run -a ruby x-retrofit-manager-MINIMUM.rb 1234 user@email.com "HUB" "USA_IL_Chicago-OHare.Intl.AP.725300_TMY3" SmallOffice WholeBuilding 3 1000 “Metal” "Concrete Mass" 0.4 Rectangle 100 50
+# xvfb-run -a ruby x-retrofit-manager-MINIMUM.rb 1234 eebhub@email.com "JASON" "USA_IL_Chicago-OHare.Intl.AP.725300_TMY3" SmallOffice WholeBuilding 3 15000 “Metal” "Concrete Mass" 0.4 Rectangle 100 50
 # Rather than pass variables as arguments in Node Command, read them from buildingData.json ##
 
 
