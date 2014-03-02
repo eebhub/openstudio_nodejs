2ecb2174e8181557c1934896f3fc2d80fc5a823e
Nicholas Mattise <n.mattise@gmail.com>
2014-03-02 13:33:03 -0500

    energy-use routing and rendering

--- routes/energy.js
+++ routes/energy.js
@@ -9,79 +9,92 @@ var database = 'test/Output/baseline.sql';
 //SQL Parsing Functions
 function getEnergyUseData(sqlFile, fn) {
     var energy = {
-        electricity: {
-            pumps :[],
-            fans : [],
-            cooling : [],
-            interiorLighting: [],
-            interiorEquipment:[],
-            heating :[],
-            exteriorLighting:[],
-            heatRejection:[],
-            humidification:[],
-            heatingRecovery:[],
-            waterSystems:[],
-            refrigeration:[],
-            generation:[]
-        },
-        gas: [{
-            cooling : [],
-            interiorEquipment:[],
-            heating :[],
-            waterSystems:[],
-            generation:[]
-        }],
-        
-    };
-    eebSqlite3.getValuesByMonthly('END USE ENERGY CONSUMPTION ELECTRICITY MONTHLY', 'Meter', '', '%', database, function(results) {
-        energy.electricity = results;
+    electricity: {
+        pumps: [],
+        fans: [],
+        cooling: [],
+        interiorLighting: [],
+        interiorEquipment: [],
+        heating: [],
+        exteriorLighting: [],
+        heatRejection: [],
+        humidification: [],
+        heatingRecovery: [],
+        waterSystems: [],
+        exteriorEquipment: [],
+        generation: []
+    },
+    gas: {
 
-        eebSqlite3.getValuesByMonthly('END USE ENERGY CONSUMPTION NATURAL GAS MONTHLY', 'Meter', '', '%', database, function(results) {
-            energy.gas = results;
-            
-            /*energy.gas.forEach(function(row){
-                if (row.curColumnName == 'HEATING:GAS'){
+        interiorEquipment: [],
+        heating: [],
+        waterSystems: [],
+        generation: [],
+        cooling:[]
+    },
+
+};
+    eebSqlite3.getValuesByMonthly('END USE ENERGY CONSUMPTION ELECTRICITY MONTHLY', 'Meter', '', '%', database, function(electResults) {
+        electResults.forEach(function(row) {
+            if (row.curColumnName == 'HEATING:ELECTRICITY') {
+                energy.electricity.heating.push(row.value);
+            }
+            else if (row.curColumnName == 'COOLING:ELECTRICITY') {
+                energy.electricity.cooling.push(row.value);
+            }
+            else if (row.curColumnName == 'PUMPS:ELECTRICITY') {
+                energy.electricity.pumps.push(row.value);
+            }
+            else if (row.curColumnName == 'FANS:ELECTRICITY') {
+                energy.electricity.fans.push(row.value);
+            }
+            else if (row.curColumnName == 'INTERIORLIGHTS:ELECTRICITY') {
+                energy.electricity.interiorLighting.push(row.value);
+            }
+            else if (row.curColumnName == 'INTERIOREQUIPMENT:ELECTRICITY') {
+                energy.electricity.interiorEquipment.push(row.value);
+            }
+            else if (row.curColumnName == 'EXTERIORLIGHTS:ELECTRICITY') {
+                energy.electricity.exteriorLighting.push(row.value);
+            }
+            else if (row.curColumnName == 'HEATREJECTION:ELECTRICITY') {
+                energy.electricity.heatRejection.push(row.value);
+            }
+            else if (row.curColumnName == 'EXTERIOREQUIPMENT:ELECTRICITY') {
+                energy.electricity.exteriorEquipment.push(row.value);
+            }
+            else if (row.curColumnName == 'HUMIDIFIER:ELECTRICITY') {
+                energy.electricity.humidification.push(row.value);
+            }
+            else if (row.curColumnName == 'HEATRECOVERY:ELECTRICITY') {
+                energy.electricity.heatingRecovery.push(row.value);
+            }
+            else if (row.curColumnName == 'WATERSYSTEMS:ELECTRICITY') {
+                energy.electricity.waterSystems.push(row.value);
+            }
+            else if (row.curColumnName == 'COGENERATION:ELECTRICITY') {
+                energy.electricity.generation.push(row.value);
+            }
+        });
+
+        eebSqlite3.getValuesByMonthly('END USE ENERGY CONSUMPTION NATURAL GAS MONTHLY', 'Meter', '', '%', database, function(gasResults) {
+            gasResults.forEach(function(row) {
+                if (row.curColumnName == 'HEATING:GAS') {
                     energy.gas.heating.push(row.value);
-                }else if(row.curColumnName == 'COOLING:GAS'){
-                     energy.gas.cooling.push(row.value);
-                }else if(row.curColumnName == 'INTERIOREQUIPMENT:GAS'){
-                     energy.gas.interiorEquipment.push(row.value);
-                }else if(row.curColumnName == 'WATERSYSTEMS:GAS'){
-                     energy.gas.waterSystems.push(row.value);
-                }else {
-                     energy.gas.generation.push(row.value);
                 }
-            });  
-            energy.electricity.forEach(function(row){
-                if (row.curColumnName == 'HEATING:ELECTRICITY'){
-                    energy.electricity.heating.push(row.value);
-                }else if(row.curColumnName == 'COOLING:ELECTRICITY'){
-                     energy.electricity.cooling.push(row.value);
-                }else if(row.curColumnName == 'PUMPS:ELECTRICITY'){
-                     energy.electricity.pumps.push(row.value);
-                }else if(row.curColumnName == 'FANS:ELECTRICITY'){
-                     energy.electricity.fans.push(row.value);
-                }else if(row.curColumnName == 'INTERIORLIGHTS:ELECTRICITY'){
-                     energy.electricity.interiorLighting.push(row.value);
-                }else if(row.curColumnName == 'INTERIOREQUIPMENT:ELECTRICITY'){
-                     energy.electricity.interiorEquipment.push(row.value);
-                }else if(row.curColumnName == 'EXTERIORLIGHTING:ELECTRICITY'){
-                     energy.electricity.exteriorLighting.push(row.value);
-                }else if(row.curColumnName == 'HEATREJECTION:ELECTRICITY'){
-                     energy.electricity.heatRejection.push(row.value);
-                }else if(row.curColumnName == 'HUMIDIFIER:ELECTRICITY'){
-                     energy.electricity.humidification.push(row.value);
-                }else if(row.curColumnName == 'HEATRECOVERY:ELECTRICITY'){
-                     energy.electricity.heatingRecovery.push(row.value);
-                }else if(row.curColumnName == 'WATERSYSTEMS:ELECTRICITY'){
-                     energy.electricity.waterSystems.push(row.value);
-                }else if(row.curColumnName == 'REFIGERATION:ELECTRICITY'){
-                     energy.electricity.refrigeration.push(row.value);
-                }else {
-                     energy.electricity.generation.push(row.value);
+                else if (row.curColumnName == 'COOLING:GAS') {
+                    energy.gas.cooling.push(row.value);
                 }
-            });  */
-            
+                else if (row.curColumnName == 'INTERIOREQUIPMENT:GAS') {
+                    energy.gas.interiorEquipment.push(row.value);
+                }
+                else if (row.curColumnName == 'WATERSYSTEMS:GAS') {
+                    energy.gas.waterSystems.push(row.value);
+                }
+                else if (row.curColumnName == 'COGENERATION:GAS') {
+                    energy.gas.generation.push(row.value);
+                }
+            });
             fn(energy);
 
         });
@@ -95,7 +108,7 @@ module.exports = {
     getEnergyUse: function(request, response) {
 
         getEnergyUseData(database, function(energy) {
-            
+
             response.render('energy-use', {
                 energy: energy
             });
--- test/test.js
+++ test/test.js
@@ -3,38 +3,79 @@ var database = 'Output/baseline.sql';
 var fs = require('fs');
 
 
- var energy = {
-        electricity: {
-            pumps :[],
-            fans : [],
-            cooling : [],
-            interiorLighting: [],
-            interiorEquipment:[],
-            heating :[],
-            exteriorLighting:[],
-            heatRejection:[],
-            humidification:[],
-            heatingRecovery:[],
-            waterSystems:[],
-            refrigeration:[],
-            generation:[]
-        },
-        gas: [{
-           
-            interiorEquipment:[],
-            heating :[],
-            waterSystems:[],
-            generation:[]
-        }],
-        
-    };
+var energy = {
+    electricity: {
+        pumps: [],
+        fans: [],
+        cooling: [],
+        interiorLighting: [],
+        interiorEquipment: [],
+        heating: [],
+        exteriorLighting: [],
+        heatRejection: [],
+        humidification: [],
+        heatingRecovery: [],
+        waterSystems: [],
+        exteriorEquipment: [],
+        generation: []
+    },
+    gas: {
 
-eebSqlite3.getValuesByMonthly('END USE ENERGY CONSUMPTION ELECTRICITY MONTHLY', 'Meter', '', '%', database, function(results) {
-    energy.electricity = results;
+        interiorEquipment: [],
+        heating: [],
+        waterSystems: [],
+        generation: [],
+        cooling:[]
+    },
 
-    eebSqlite3.getValuesByMonthly('END USE ENERGY CONSUMPTION NATURAL GAS MONTHLY', 'Meter', '', '%', database, function(results) {
-        energy.gas[0] = results;
-        energy.gas.forEach(function(row){
+};
+
+
+eebSqlite3.getValuesByMonthly('END USE ENERGY CONSUMPTION ELECTRICITY MONTHLY', 'Meter', '', '%', database, function(electResults) {
+    electResults.forEach(function(row) {
+        if (row.curColumnName == 'HEATING:ELECTRICITY') {
+            energy.electricity.heating.push(row.value);
+        }
+        else if (row.curColumnName == 'COOLING:ELECTRICITY') {
+            energy.electricity.cooling.push(row.value);
+        }
+        else if (row.curColumnName == 'PUMPS:ELECTRICITY') {
+            energy.electricity.pumps.push(row.value);
+        }
+        else if (row.curColumnName == 'FANS:ELECTRICITY') {
+            energy.electricity.fans.push(row.value);
+        }
+        else if (row.curColumnName == 'INTERIORLIGHTS:ELECTRICITY') {
+            energy.electricity.interiorLighting.push(row.value);
+        }
+        else if (row.curColumnName == 'INTERIOREQUIPMENT:ELECTRICITY') {
+            energy.electricity.interiorEquipment.push(row.value);
+        }
+        else if (row.curColumnName == 'EXTERIORLIGHTS:ELECTRICITY') {
+            energy.electricity.exteriorLighting.push(row.value);
+        }
+        else if (row.curColumnName == 'HEATREJECTION:ELECTRICITY') {
+            energy.electricity.heatRejection.push(row.value);
+        }
+        else if (row.curColumnName == 'EXTERIOREQUIPMENT:ELECTRICITY') {
+            energy.electricity.exteriorEquipment.push(row.value);
+        }
+        else if (row.curColumnName == 'HUMIDIFIER:ELECTRICITY') {
+            energy.electricity.humidification.push(row.value);
+        }
+        else if (row.curColumnName == 'HEATRECOVERY:ELECTRICITY') {
+            energy.electricity.heatingRecovery.push(row.value);
+        }
+        else if (row.curColumnName == 'WATERSYSTEMS:ELECTRICITY') {
+            energy.electricity.waterSystems.push(row.value);
+        }
+        else if(row.curColumnName == 'COGENERATION:ELECTRICITY'){
+            energy.electricity.generation.push(row.value);
+        }
+    });
+   
+    eebSqlite3.getValuesByMonthly('END USE ENERGY CONSUMPTION NATURAL GAS MONTHLY', 'Meter', '', '%', database, function(gasResults) {
+      gasResults.forEach(function(row){
                 if (row.curColumnName == 'HEATING:GAS'){
                     energy.gas.heating.push(row.value);
                 }else if(row.curColumnName == 'COOLING:GAS'){
@@ -43,41 +84,16 @@ eebSqlite3.getValuesByMonthly('END USE ENERGY CONSUMPTION ELECTRICITY MONTHLY', 
                      energy.gas.interiorEquipment.push(row.value);
                 }else if(row.curColumnName == 'WATERSYSTEMS:GAS'){
                      energy.gas.waterSystems.push(row.value);
-                }else {
+                }else if(row.curColumnName == 'COGENERATION:GAS'){
                      energy.gas.generation.push(row.value);
                 }
-            }); 
-            energy.electricity.forEach(function(row){
-                if (row.curColumnName == 'HEATING:ELECTRICITY'){
-                    energy.electricity.heating.push(row.value);
-                }else if(row.curColumnName == 'COOLING:ELECTRICITY'){
-                     energy.electricity.cooling.push(row.value);
-                }else if(row.curColumnName == 'PUMPS:ELECTRICITY'){
-                     energy.electricity.pumps.push(row.value);
-                }else if(row.curColumnName == 'FANS:ELECTRICITY'){
-                     energy.electricity.fans.push(row.value);
-                }else if(row.curColumnName == 'INTERIORLIGHTS:ELECTRICITY'){
-                     energy.electricity.interiorLighting.push(row.value);
-                }else if(row.curColumnName == 'INTERIOREQUIPMENT:ELECTRICITY'){
-                     energy.electricity.interiorEquipment.push(row.value);
-                }else if(row.curColumnName == 'EXTERIORLIGHTING:ELECTRICITY'){
-                     energy.electricity.exteriorLighting.push(row.value);
-                }else if(row.curColumnName == 'HEATREJECTION:ELECTRICITY'){
-                     energy.electricity.heatRejection.push(row.value);
-                }else if(row.curColumnName == 'HUMIDIFIER:ELECTRICITY'){
-                     energy.electricity.humidification.push(row.value);
-                }else if(row.curColumnName == 'HEATRECOVERY:ELECTRICITY'){
-                     energy.electricity.heatingRecovery.push(row.value);
-                }else if(row.curColumnName == 'WATERSYSTEMS:ELECTRICITY'){
-                     energy.electricity.waterSystems.push(row.value);
-                }else if(row.curColumnName == 'REFIGERATION:ELECTRICITY'){
-                     energy.electricity.refrigeration.push(row.value);
-                }else {
-                     energy.electricity.generation.push(row.value);
-                }
-            });  
-        fs.writeFile("energy.json", JSON.stringify(energy, null, 4), function(err) {
+            });
+        
+        fs.writeFile("energyFull.json", JSON.stringify(energy, null, 4), function(err) {
             if (err) console.log("Error");
         });
+
+
+
     });
 });
