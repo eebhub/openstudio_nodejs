bbdd00124c691824d73850360c425aa02c69b71d
Josh Wentz <joshwentz@gmail.com>
2014-03-04 19:21:18 -0500

    Show folders & files of jason folder like an Apache Web Server

--- app.js
+++ app.js
@@ -22,6 +22,11 @@ app.use(express.methodOverride());
 app.use(app.router);
 app.use(express.static(path.join(__dirname, 'public')));
 
+//Show Folders & Files like Apache
+app.use(express.directory('public'));
+app.use('/outputs', express.directory('jason', {icons:true}));
+app.use('/outputs', express.static('jason'));
+
 // development only
 if ('development' == app.get('env')) {
   app.use(express.errorHandler());
