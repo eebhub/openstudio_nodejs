3b91a8d0f0cf899ea50ff65a1a4e7ca1c86f6046
Nicholas Mattise <n.mattise@gmail.com>
2014-03-14 13:33:17 -0400

    test files

--- test/test.js
+++ test/test.js
@@ -1,99 +1,51 @@
-var eebSqlite3 = require('../lib/eeb_sqlite3.js');
+var sqlite3 = require('sqlite3').verbose();
 var database = 'Output/baseline.sql';
 var fs = require('fs');
+var number_format = require('../lib/number_format.js');
 
 
-var energy = {
-    electricity: {
-        pumps: [],
-        fans: [],
-        cooling: [],
-        interiorLighting: [],
-        interiorEquipment: [],
-        heating: [],
-        exteriorLighting: [],
-        heatRejection: [],
-        humidification: [],
-        heatingRecovery: [],
-        waterSystems: [],
-        exteriorEquipment: [],
-        generation: []
-    },
-    gas: {
+//Values by Month
+function getValuesByMonth(reportName, reportForString, tableName, units, db_path, fn) {
+    var db = new sqlite3.Database(db_path);
+    var sql = 'Select Distinct * From TabularDataWithStrings Where ReportName Like "' + reportName + '" And ReportForString Like "' + reportForString + '" and TableName = "' + tableName + '" and Units Like "' + units + '"';
+    var results = [];
+    
+    db.all(sql, function(err, rows) {
+           rows.forEach(function(curRow){
+              results.push(curRow);
+           });
+            //console.log(results);
+            fn(results);
+            //return may not work due to the asynchronous
+        }); 
+}
 
-        interiorEquipment: [],
-        heating: [],
-        waterSystems: [],
-        generation: [],
-        cooling:[]
-    },
+function getValues(reportName, reportForString, tableName, units, db_path, fn) {
+        var db = new sqlite3.Database(db_path);
 
-};
+        var sql = 'Select Distinct * From TabularDataWithStrings Where ReportName Like "' + reportName + '" And ReportForString Like "' + reportForString + '" and TableName = "' + tableName + '" and Units Like "' + units + '"';
+        var results = [];
 
-
-eebSqlite3.getValuesByMonthly('END USE ENERGY CONSUMPTION ELECTRICITY MONTHLY', 'Meter', '', '%', database, function(electResults) {
-    electResults.forEach(function(row) {
-        if (row.curColumnName == 'HEATING:ELECTRICITY') {
-            energy.electricity.heating.push(row.value);
-        }
-        else if (row.curColumnName == 'COOLING:ELECTRICITY') {
-            energy.electricity.cooling.push(row.value);
-        }
-        else if (row.curColumnName == 'PUMPS:ELECTRICITY') {
-            energy.electricity.pumps.push(row.value);
-        }
-        else if (row.curColumnName == 'FANS:ELECTRICITY') {
-            energy.electricity.fans.push(row.value);
-        }
-        else if (row.curColumnName == 'INTERIORLIGHTS:ELECTRICITY') {
-            energy.electricity.interiorLighting.push(row.value);
-        }
-        else if (row.curColumnName == 'INTERIOREQUIPMENT:ELECTRICITY') {
-            energy.electricity.interiorEquipment.push(row.value);
-        }
-        else if (row.curColumnName == 'EXTERIORLIGHTS:ELECTRICITY') {
-            energy.electricity.exteriorLighting.push(row.value);
-        }
-        else if (row.curColumnName == 'HEATREJECTION:ELECTRICITY') {
-            energy.electricity.heatRejection.push(row.value);
-        }
-        else if (row.curColumnName == 'EXTERIOREQUIPMENT:ELECTRICITY') {
-            energy.electricity.exteriorEquipment.push(row.value);
-        }
-        else if (row.curColumnName == 'HUMIDIFIER:ELECTRICITY') {
-            energy.electricity.humidification.push(row.value);
-        }
-        else if (row.curColumnName == 'HEATRECOVERY:ELECTRICITY') {
-            energy.electricity.heatingRecovery.push(row.value);
-        }
-        else if (row.curColumnName == 'WATERSYSTEMS:ELECTRICITY') {
-            energy.electricity.waterSystems.push(row.value);
-        }
-        else if(row.curColumnName == 'COGENERATION:ELECTRICITY'){
-            energy.electricity.generation.push(row.value);
-        }
-    });
-   
-    eebSqlite3.getValuesByMonthly('END USE ENERGY CONSUMPTION NATURAL GAS MONTHLY', 'Meter', '', '%', database, function(gasResults) {
-      gasResults.forEach(function(row){
-                if (row.curColumnName == 'HEATING:GAS'){
-                    energy.gas.heating.push(row.value);
-                }else if(row.curColumnName == 'COOLING:GAS'){
-                     energy.gas.cooling.push(row.value);
-                }else if(row.curColumnName == 'INTERIOREQUIPMENT:GAS'){
-                     energy.gas.interiorEquipment.push(row.value);
-                }else if(row.curColumnName == 'WATERSYSTEMS:GAS'){
-                     energy.gas.waterSystems.push(row.value);
-                }else if(row.curColumnName == 'COGENERATION:GAS'){
-                     energy.gas.generation.push(row.value);
-                }
+        db.all(sql, function(err, rows) {
+            rows.forEach(function(row) {
+                results.push(row);
             });
-        
-        fs.writeFile("energyFull.json", JSON.stringify(energy, null, 4), function(err) {
-            if (err) console.log("Error");
+            //console.log(results);
+            fn(results);
+            //return may not work due to the asynchronous
         });
+        db.close;
 
-
-
-    });
+    }
+//Energy-Use
+getValuesByMonth('END USE ENERGY CONSUMPTION ELECTRICITY MONTHLY', 'Meter', '', '%', database, function(results){
+    console.log(results);
+});
+//Energy-Cost
+getValues('Tariff Report', 'BLDG101_ELECTRIC_RATE', 'Categories', '%',database, function(results){
+    console.log(results);
+});
+//Energy-Intensity
+getValues('AnnualBuildingUtilityPerformanceSummary', 'Entire Facility', 'Site and Source Energy', '%', database, function(results){
+    console.log(results);
 });
