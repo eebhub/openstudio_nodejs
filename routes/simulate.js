8d6830f3809d281a4dce76b1069adc860c6def03
Josh Wentz <joshwentz@gmail.com>
2014-03-16 23:57:23 -0400

    Comment out openstudio-run & openstudio-model until ready (otherwise broke node app

--- routes/simulate.js
+++ routes/simulate.js
@@ -1,12 +1,15 @@
 //DEPENDENCIES
 var fs = require("fs");
-var openstudioRun = require("../library/openstudio-run.js");
-var openstudioModel = require("../library/openstudio-model.js");
+//var openstudioRun = require("../library/openstudio-run.js");
+//var openstudioModel = require("../library/openstudio-model.js");
 
 //SIMULATE OPENSTUDIO
 module.exports = {openstudio: function(request, response) {
-
+    
+    console.log('hello from simulate.js');
     console.log(request.body);
+    
+    response.redirect('energy-use');
 
 }//end openstudio
 };//end exports
