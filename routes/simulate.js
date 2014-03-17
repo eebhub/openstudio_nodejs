4b7ecd023404e92918c9770115e4697d6cd02e2f
Josh Wentz <joshwentz@gmail.com>
2014-03-17 13:04:56 -0400

    Simulate logic for OpenStudio & EnergyPlus in Node Express, Create Unique Name & Folder

--- routes/simulate.js
+++ routes/simulate.js
@@ -1,5 +1,6 @@
 //DEPENDENCIES
-var fs = require("fs");
+var fs = require("fs"); //Nodejs File System
+var timestp = require("../library/timestamp.js"); //Timestamp code
 //var openstudioRun = require("../library/openstudio-run.js");
 //var openstudioModel = require("../library/openstudio-model.js");
 
@@ -9,6 +10,27 @@ module.exports = {openstudio: function(request, response) {
     console.log('hello from simulate.js');
     console.log(request.body);
     
+    //CREATE unique simulation Name & Timestamp
+    var buildingName = request.body.buildingName.replace(/\s+/g, '') || "NoName";
+    console.log(buildingName);
+    var timestamp = timestp.createTimestamp();
+    var buildingNameTimestamp =  "TEST_"+buildingName+timestamp;
+    
+    //CREATE unique simulation Folder
+    var outputPath = "../simulations/" + buildingNameTimestamp;
+    fs.mkdirSync(outputPath, function(error) {if (error) throw error;}); //simulation_directory in openstudio-model.js
+
+    //FORMAT request.body json to match buildingData2.json 
+    
+    
+    //SAVE formatted json to outputPath with name buildingNameTimestamp_input.json
+    
+    
+    //RUN openstudio-run.js & openstudio-model.js
+    
+    //APPEND important energyplus output sql tables into original json
+    
+    //RENDER EnergyPlus Graphs & Files outputs.ejs
     response.redirect('energy-use');
 
 }//end openstudio
