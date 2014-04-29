d7adc13ba8d57e1464c13fe584e154d57733d402
Josh Wentz <joshwentz@gmail.com>
2014-04-29 14:09:32 -0400

    Revert setting Building Name for EnergyPlus until bug fixed

--- library/openstudio-model.js
+++ library/openstudio-model.js
@@ -13,9 +13,9 @@ function OpenStudioModel(building, runmanager) {
   console.log("Constructed openstudio.model.Model()");
   
   //Set Building Name in OpenStudio/EnergyPlus
-  var buildingName = building.buildingInfo.buildingName;
-  openstudio.model.getBuilding(this.model).setName(buildingName); 
-  console.log("EnergyPlus Building Name: "+buildingName);
+//   var buildingName = building.buildingInfo.buildingName;
+//   openstudio.model.getBuilding(this.model).setName(buildingName); 
+//   console.log("EnergyPlus Building Name: "+buildingName);
 
   this.add_geometry_rectangle = function(architecture) {
     var length = architecture.buildingLength;
